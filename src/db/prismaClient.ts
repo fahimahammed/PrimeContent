9553d944731ff3680dc9d345287c42e96149743f
fahimahammed <fahimahammed.cse@gmail.com>
2024-08-17 01:07:43 +0600

    :bug: Add conditional query logging for development environment

--- src/db/prismaClient.ts
+++ src/db/prismaClient.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from '@prisma/client';
+import config from '../config';
 
 // Initialize Prisma Client
 const prisma = new PrismaClient({
@@ -15,15 +16,17 @@ const prisma = new PrismaClient({
 //     console.log('\x1b[32m%s\x1b[0m', 'Params: ', JSON.stringify(e.params, null, 2)); // Green color for 'Params:', with pretty-printed JSON
 //     console.log('\x1b[35m%s\x1b[0m', 'Duration: ', e.duration, ' ms'); // Magenta color for 'Duration:'
 // });
-prisma.$on('query', (e) => {
-    const border = '- '.repeat(80); // Adjust the length as needed
-    const margin = ' '.repeat(4); // Adjust the margin as needed
+if (config.environment === "development") {
+    prisma.$on('query', (e) => {
+        const border = '- '.repeat(80); // Adjust the length as needed
+        const margin = ' '.repeat(4); // Adjust the margin as needed
 
-    console.log(`\x1b[36m${border}\x1b[0m`); // Cyan border
-    console.log(`\x1b[36m${margin}Query: \x1b[0m%s`, e.query); // Cyan color for 'Query:' with margin
-    console.log(`\x1b[32m${margin}Params: \x1b[0m%s`, e.params); // Green color for 'Params:' with margin
-    console.log(`\x1b[35m${margin}Duration: \x1b[0m%d ms`, e.duration); // Magenta color for 'Duration:' with margin
-    console.log(`\x1b[36m${border}\x1b[0m`); // Cyan border
-});
+        console.log(`\x1b[36m${border}\x1b[0m`); // Cyan border
+        console.log(`\x1b[36m${margin}Query: \x1b[0m%s`, e.query); // Cyan color for 'Query:' with margin
+        console.log(`\x1b[32m${margin}Params: \x1b[0m%s`, e.params); // Green color for 'Params:' with margin
+        console.log(`\x1b[35m${margin}Duration: \x1b[0m%d ms`, e.duration); // Magenta color for 'Duration:' with margin
+        console.log(`\x1b[36m${border}\x1b[0m`); // Cyan border
+    });
+};
 
 export default prisma;
